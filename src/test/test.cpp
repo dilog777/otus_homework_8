16840f803df1ff70896ec938eaf9a1d006b760bf
dilog777 <132608049+dilog777@users.noreply.github.com>
2023-07-30 22:00:16 +0300

    Refactoring

--- src/test/test.cpp
+++ src/test/test.cpp
@@ -3,12 +3,8 @@
 #include "lib.h"
 
 
+
 TEST(test_google_version, test_valid_build)
 {
 	EXPECT_TRUE(buildNumber() > 0);
-}
-
-int main(int argc, char** argv) {
-	testing::InitGoogleTest(&argc, argv);
-	return RUN_ALL_TESTS();
 }
